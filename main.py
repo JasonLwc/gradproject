c535757d978057de9b2e0735046d38f1b478f9e2
JasonLwc <167592649+JasonLwc@users.noreply.github.com>
2024-07-16 23:23:52 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,135 @@
+# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
+import os
+import streamlit as st
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFaceHub
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.runnables import RunnablePassthrough
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import FireCrawlLoader
+from langchain_community.vectorstores.utils import filter_complex_metadata
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+load_dotenv()
+
+#os.environ['TOGEHTERAI_KEY'] ='396bfe530e2c2f5a8360f73645aa7568c238f74b617506f23467feb6b8f06b62'
+
+
+
+
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = FireCrawlLoader(
+        api_key="fc-a172a6abb47a4cea90d49453f71deb2d",
+        url=url,
+        mode="crawl",
+        params={
+            'crawlerOptions': {
+                'limit': 4,  # Set maximum number of pages to 4
+            }
+        }
+    )
+    raw_documents = loader.load()
+    print("Raw documents:", raw_documents)  # Add this line
+
+    # Ensure all raw_documents are strings
+    documents = [Document(page_content=str(text), metadata={}) for text in raw_documents]
+    
+    # Create Document objects
+    #documents = [Document(page_content=text, metadata={}) for text in raw_documents]
+    
+    # split the documents into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(documents)
+    
+    # create a vectorstore from the chunks
+    hfe = HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5")
+    vector_store = Chroma.from_documents(document_chunks, hfe)
+
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI(
+        base_url="https://api.together.xyz/v1",
+        api_key=os.environ["TOGETHERAI_KEY"],
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",)
+
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI(
+        base_url="https://api.together.xyz/v1",
+        api_key=os.environ["TOGETHERAI_KEY"],
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",)
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="⭐")
+st.title("Chat with websites")
+# sidebar
+with st.sidebar:
+    st.header("Settings")
+    website_url = st.text_input("Website URL")
+
+if not website_url:
+    st.info("Please enter a website URL")
+
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Yo, whazz up. What can I do for you today?"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
+    user_query = st.chat_input("Give me your wish here...")
+    if user_query:
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
+
+    # conversation
+    for message in st.session_state.chat_history:
+        with st.chat_message("AI" if isinstance(message, AIMessage) else "Human"):
+            st.write(message.content)
